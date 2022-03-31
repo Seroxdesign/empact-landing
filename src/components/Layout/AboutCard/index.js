6d7cdb4eaf92cc99db8d042440c8df895eb2932f
sky cherfa <sherifcherfa@gmail.com>
2022-03-31 19:11:07 -0400

    opened about page

---
+++ src/components/Layout/AboutCard/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+export default function AboutCard() {
+  return (
+    <div className={styles.card}>
+      <div>
+        <img src={""} alt={""} className={""}></img>
+        <h3 className={styles.title}></h3>
+        <p className={styles.para}></p>
+      </div>
+     
+      <div>
+        
+      </div>
+    </div>
+  )
+}
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import AboutCard from '../../components/Layout/AboutCard'
 import { Link } from 'react-router-dom'
+import styles from './styles.module.scss'
 
 export default function About() {
   return (
-    <div>Coming soon
-      <Link to={"/"}>Back</Link>
+    <div className={styles.about_wrapper}>
+      <h2 className={styles.title}>Meet the devs</h2>
+
+      <div className={styles.dev_grid}>
+        <AboutCard />
+        <AboutCard />
+        <AboutCard />
+      </div>
     </div>
   )
 }
