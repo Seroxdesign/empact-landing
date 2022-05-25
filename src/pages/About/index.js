3114c99bd668874b1d21d6a2f5ac74cf9d4f4755
Sero <69639595+Seroxdesign@users.noreply.github.com>
2022-05-25 09:54:35 -0400

    Added navbar to about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutCard from '../../components/Layout/AboutCard'
 import { introductions } from '../../constants/data/introductions'
+import Navbar from '../../components/Layout/Navbar'
 import { Link } from 'react-router-dom'
 import styles from './styles.module.scss'
 
 export default function About() {
   return (
     <div className={styles.about_wrapper}>
+     <Navbar />
       <div className={styles.top}>
         <h2 className={styles.title}>Meet the Team</h2>
 
